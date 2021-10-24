009a661e8fe813a286f6e94fa2843d6e8523f367
Andrei Zhozhin <andrei.zhozhin@gmail.com>
2021-10-24 22:36:10 +0100

    switch from eureka+cloud config server to consul

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -14,12 +14,12 @@ docker-compose up -d grafana-server prometheus-server tracing-server
 
 echo "Running apps"
 mkdir -p target
-nohup java -jar spring-petclinic-config-server/target/*.jar --server.port=8888 --spring.profiles.active=chaos-monkey > target/config-server.log 2>&1 &
-echo "Waiting for config server to start"
-sleep 20
-nohup java -jar spring-petclinic-discovery-server/target/*.jar --server.port=8761 --spring.profiles.active=chaos-monkey > target/discovery-server.log 2>&1 &
-echo "Waiting for discovery server to start"
-sleep 20
+#nohup java -jar spring-petclinic-config-server/target/*.jar --server.port=8888 --spring.profiles.active=chaos-monkey > target/config-server.log 2>&1 &
+#echo "Waiting for config server to start"
+#sleep 20
+#nohup java -jar spring-petclinic-discovery-server/target/*.jar --server.port=8761 --spring.profiles.active=chaos-monkey > target/discovery-server.log 2>&1 &
+#echo "Waiting for discovery server to start"
+#sleep 20
 nohup java -jar spring-petclinic-customers-service/target/*.jar --server.port=8081 --spring.profiles.active=chaos-monkey > target/customers-service.log 2>&1 &
 nohup java -jar spring-petclinic-visits-service/target/*.jar --server.port=8082 --spring.profiles.active=chaos-monkey > target/visits-service.log 2>&1 &
 nohup java -jar spring-petclinic-vets-service/target/*.jar --server.port=8083 --spring.profiles.active=chaos-monkey > target/vets-service.log 2>&1 &
